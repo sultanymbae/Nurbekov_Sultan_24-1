03090f4420d9717efe94104dd03002b9ba025860
Your Name <sultannurbekov12@gmail.com>
2023-01-13 16:58:50 +0600

    hw1

--- InternetShop/urls.py
+++ InternetShop/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from posts.views import hello, now_date, goodbye
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello),
+    path('nowdate/', now_date),
+    path('goodbye/', goodbye)
 ]
